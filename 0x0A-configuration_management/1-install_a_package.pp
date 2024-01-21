1723eb0060edf763b05ccf65560f7a5ff0f2cf80
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-21 00:01:56 +0000

    All tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# Download puppet-lint 2.1.1
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
-}
-
-package { 'rubygems1.9.1':
-  ensure  => 'installed',
-  require => Exec['apt-get update']
-}
-
-package { 'puppet-lint':
-  ensure   => '2.1.1'
-  require  => Package['rubygems1.9.1'],
-  provider => 'gem'
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
