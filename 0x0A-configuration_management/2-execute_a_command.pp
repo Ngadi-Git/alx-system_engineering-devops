3974ddbddfb3c60b7b3f11db0d74c0537e9b0df9
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-01-23 20:02:07 +0100

    Creating file using puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
-# This manifest creates a file at /tmp
-file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+# Executes a bash command
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
