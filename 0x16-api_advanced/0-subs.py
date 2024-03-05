b746ea9e7803a23de2dba7369872b03ec8b60951
Ngadi-Git <ngadi.l.paul@gmail.com>
2024-03-05 13:10:03 +0100

    Reddit api request functions added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Function to GET number of subscribers for a given subreddit"""
-from requests import get
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Use GET request to find number of subscribers to `subreddit`"""
-    r = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-            params={"raw_json": 1},
-            headers={"User-Agent": "Andrew from Holberton"},
-            allow_redirects=False)
-
-    try:
-        r.raise_for_status()
-    except:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        num_subscribers = r.json().get('data').get('subscribers')
-        if num_subscribers is None:
-            return 0
-        return num_subscribers
+    results = response.json().get("data")
+    return results.get("subscribers")
